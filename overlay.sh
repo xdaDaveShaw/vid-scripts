bfcc49a339ac8c0d512b89b6e9a92e108b7a10a4
Dave Shaw <dave@taeguk.co.uk>
2023-03-14 23:14:35 +0000

    Update overlay

--- overlay.sh
+++ overlay.sh
@@ -5,8 +5,8 @@ then
         echo "Pass {filename}"
         echo ""
 else
-	ffmpeg -i "$1" -i Overlay-Sm.png \
-	-filter_complex "[0:v][1:v] overlay=0:H-h" \
+	ffmpeg -i "$1" -i Overlay-Top-Med.png \
+	-filter_complex "[0:v][1:v] overlay=0:0" \
 	-c:a copy \
 	"$1-overlaid.mp4"
 	#-pix_fmt yuv420p \
