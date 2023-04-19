92503e91e538dac2323a4499f3f44acb4a1d7004
Dave Shaw <dave@taeguk.co.uk>
2023-04-19 09:35:43 +0100

    auto-trim script

---
+++ auto-trim.sh
@@ -0,0 +1,20 @@
+./trim-vid.sh 1.mp4 "2:55" "2:05"
+mv trimmed.mp4 1t.mp4
+
+./trim-vid.sh 2.mp4 "0:40" "0:40"
+mv trimmed.mp4 2ta.mp4
+./trim-vid.sh 2.mp4 "4:00" "1:00"
+mv trimmed.mp4 2tb.mp4
+
+./trim-vid.sh 3.mp4 "0:40" "1:00"
+mv trimmed.mp4 3ta.mp4
+./trim-vid.sh 3.mp4 "4:00" "1:00"
+mv trimmed.mp4 3tb.mp4
+
+./trim-vid.sh 4.mp4 "2:00" "1:40"
+mv trimmed.mp4 4t.mp4
+
+./trim-vid.sh 5.mp4 "0:00" "1:20"
+mv trimmed.mp4 5ta.mp4
+./trim-vid.sh 5.mp4 "2:40" "2:00"
+mv trimmed.mp4 5tb.mp4
